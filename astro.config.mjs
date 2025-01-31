ebeef394c7d402a12709300d1a85093f27ce29bd
Finn Bartels <fibart@gmx.de>
2025-01-31 14:38:09 +0100

    add global styles, integrate React, and enhance layout with AuthorCard component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      tailwind({
-
-      }),
-
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react()],
   output: 'server',
   adapter: node({ mode: 'standalone'}),
 });
